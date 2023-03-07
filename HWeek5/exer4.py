f12b0dab4d139e812caff314cc67c02ab3e1b1d6
Farabi Kuanysh <f_kuanysh@kbtu.kz>
2023-03-07 14:41:57 +0600

    FFF

--- HWeek5/exer4.py
+++ HWeek5/exer4.py
@@ -1,6 +1,6 @@
 import re
 
 txt = str(input())
-x = re.match("[A-Z][a-z]", txt)
+x = re.match("[A-Z]{1}[a-z]", txt)
 
 print(x)
--- HWeek5/exer5.py
+++ HWeek5/exer5.py
@@ -1,9 +1,6 @@
 import re
 
 txt = str(input())
-x = re.findall("^a(.)*b$", txt)
+x = re.findall("a.*b", txt)
 
-if x:
-  print("YES! We have a match!")
-else:
-  print("No match")
+print (x)
